b50c49fa28a9d64f4c5bd49dc57f787042b2fb96
Prajit <prajitramesh123@gmail.com>
2026-02-17 23:50:50 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+const port=8000;
+
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send('Hello from express server');
+})
+
+app.listen(port,()=>{
+    console.log(`Server running at port ${port}`);
+})
